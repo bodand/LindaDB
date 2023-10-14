4870e716a0ecd1cb290c3eb2a7e042a6e0cb2d35
bodand <bodand@proton.me>
2023-10-14 20:03:16 +0200

    feat: initial project config
    - added vcpkg submodule
    - configured project via init.pl
    - purged release related CI tooling, this project only produces static libraries which are intended to be used via vcpkg in the main Linda library
    - purged release related CMake orchestration (see previous point)
    - vcpkg: added mimalloc dependency
    - vcpkg: added Catch2 dependency

--- ci/package_env.bat
+++
@@ -1,4 +0,0 @@
-@REM Windows is fix, we don't need to emulate the `uname` part
-cd %BUILD_PATH%
-call vars.cmd
-for /f "usebackq" %%a in (`winarch`) do echo:set "CG_PKG_NAME=cg3-checker-%CG_VER%-Windows-%%a" >> vars.cmd
--- ci/sha256.ps1
+++
@@ -1,4 +0,0 @@
-param ($file)
-
-$hash = Get-FileHash $file
-Write-Host -NoNewline $hash.Algorithm "($file)" = $hash.Hash
