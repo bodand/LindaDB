567a0114f1852c7727d433a67a37a949c6f30a4a
bodand <bodand@proton.me>
2023-11-22 19:23:45 +0100

    ci: try fix ci

--- ci/symlinks.sh
+++ ci/symlinks.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-if [ "x${CG_TOOLCHAIN}" = "xgcc" ]; then
+if [ "x${CG_TOOLCHAIN}" = "xgcc" ]
+then
   echo "Using gcc toolchain"
   ln -fs "$(which gcc)" /usr/bin/cc
   echo "linked cc"
@@ -8,7 +9,8 @@ if [ "x${CG_TOOLCHAIN}" = "xgcc" ]; then
   echo "linked c++"
 fi
 
-if [ "x${CG_TOOLCHAIN}" = "xclang" ]; then
+if [ "x${CG_TOOLCHAIN}" = "xclang" ]
+then
   echo "Using clang toolchain"
   ln -fs "$(which clang)" /usr/bin/cc
   echo "linked cc"
