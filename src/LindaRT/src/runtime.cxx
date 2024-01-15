32f8c1e3707deefd4804c3a885d724cefcb2a90b
bodand <bodand@proton.me>
2024-01-15 18:41:24 +0100

    refr: refactored the runtime's messaging
    - refactored the way the runtime processes incoming messages, messages are now processed based on the incoming message's MPI tag

--- src/LindaRT/src/runtime.cxx
+++ src/LindaRT/src/runtime.cxx
@@ -35,13 +35,19 @@
  */
 
 #include <algorithm>
-#include <bit>
+#include <cassert>
+#include <cstddef>
 #include <execution>
-#include <functional>
+#include <ios>
+#include <iostream>
 #include <memory>
+#include <numeric>
+#include <span>
 #include <utility>
 #include <vector>
 
+#include <ldb/bcast/broadcaster.hxx>
+#include <ldb/lv/linda_tuple.hxx>
 #include <lrt/runtime.hxx>
 #include <lrt/serialize/tuple.hxx>
 
@@ -52,6 +58,17 @@ namespace {
     constexpr const int LINDA_RT_DB_SYNC_INSERT_TAG = 0xDB'00'02;
     constexpr const int LINDA_RT_DB_SYNC_DELETE_TAG = 0xDB'00'03;
 
+    constexpr std::byte
+    payload_from_command(int tag) {
+        assert(tag > 0);
+        return static_cast<std::byte>(static_cast<unsigned>(tag) & 0xFFU);
+    }
+
+    constexpr int
+    command_from_payload(std::byte received) {
+        return static_cast<int>(static_cast<unsigned>(received) | 0xDB'00'00U);
+    }
+
     struct mpi_request_vector_awaiter final {
         mpi_request_vector_awaiter(std::vector<MPI_Request>&& reqs,
                                    std::unique_ptr<std::byte[]>&& buf)
@@ -61,53 +78,59 @@ namespace {
     private:
         friend void
         await(mpi_request_vector_awaiter& awaiter) {
+            assert(!awaiter._finished);
             MPI_Waitall(static_cast<int>(awaiter._reqs.size()),
                         awaiter._reqs.data(),
                         MPI_STATUS_IGNORE);
+            awaiter._finished = true;
+            awaiter._buf.reset();
         }
 
+        bool _finished{};
         std::vector<MPI_Request> _reqs;
         std::unique_ptr<std::byte[]> _buf;
     };
 
     static_assert(ldb::awaitable<mpi_request_vector_awaiter>);
 
-    struct bcast_handler {
+    struct bcast_handler final {
         using await_type = mpi_request_vector_awaiter;
 
-        explicit
-        bcast_handler(const int rank, const int size)
+        explicit bcast_handler(const int rank, const int size)
              : _myrank(rank),
                _size(size) { }
 
     private:
         [[nodiscard]] std::vector<MPI_Request>
         broadcast_with_tag(int tag, std::span<std::byte> bytes) const {
-            std::vector req(static_cast<std::size_t>(_size), MPI_REQUEST_NULL);
-            std::vector<int> idx(static_cast<std::size_t>(_size));
-            std::iota(idx.begin(), idx.end(), std::size_t{});
+            const auto ranks_sz = static_cast<std::size_t>(_size);
+            std::vector requests(ranks_sz, MPI_REQUEST_NULL);
+
+            std::vector<int> ranks(ranks_sz);
+            std::iota(ranks.begin(), ranks.end(), 0);
+
             std::transform(std::execution::par_unseq,
-                           idx.begin(),
-                           idx.end(),
-                           req.begin(),
+                           ranks.begin(),
+                           ranks.end(),
+                           requests.begin(),
                            [bytes, tag, my_rank = _myrank](int rank) {
                                if (rank == my_rank) return MPI_REQUEST_NULL;
 
                                MPI_Request req;
-                               const auto status = MPI_Isend(bytes.data(),
-                                                             static_cast<int>(bytes.size()),
-                                                             MPI_CHAR,
-                                                             rank,
-                                                             tag,
-                                                             MPI_COMM_WORLD,
-                                                             &req);
-                               if (status != 0) {
-                                   return MPI_REQUEST_NULL;
+                               if (const auto status = MPI_Isend(bytes.data(),
+                                                                 static_cast<int>(bytes.size()),
+                                                                 MPI_CHAR,
+                                                                 rank,
+                                                                 tag,
+                                                                 MPI_COMM_WORLD,
+                                                                 &req);
+                                   status == 0) {
+                                   return req;
                                }
-                               return req;
+                               return MPI_REQUEST_NULL;
                            });
 
-            return req;
+            return requests;
         }
 
         friend mpi_request_vector_awaiter
@@ -134,7 +157,7 @@ namespace {
 }
 
 lrt::runtime::
-runtime(int* argc, char*** argv)
+       runtime(int* argc, char*** argv)
      : _recv_thr(&lrt::runtime::recv_thread_worker, this) {
     if (!_mpi_inited.test_and_set()) {
         int got_thread = MPI_THREAD_SINGLE;
@@ -154,10 +177,9 @@ runtime(int* argc, char*** argv)
     _recv_start.notify_all();
 }
 
-lrt::runtime::~
-runtime() noexcept {
-    constexpr char buf{'\0'};
-    MPI_Send(&buf, 1, MPI_CHAR, _rank, LINDA_RT_TERMINATE_TAG, MPI_COMM_WORLD);
+lrt::runtime::~runtime() noexcept {
+    constexpr char buf{};
+    MPI_Send(&buf, 0, MPI_CHAR, _rank, LINDA_RT_TERMINATE_TAG, MPI_COMM_WORLD);
     _recv_thr.join();
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Finalize();
@@ -169,17 +191,35 @@ lrt::runtime::recv_thread_worker() {
     for (;;) {
         MPI_Status stat{};
         MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+
         int len;
         MPI_Get_count(&stat, MPI_CHAR, &len);
-        if (len == 1) {
-            char term_buf{};
-            MPI_Recv(&term_buf, 1, MPI_CHAR, MPI_ANY_SOURCE, LINDA_RT_TERMINATE_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            if (term_buf == 0) return;
-            continue;
+        auto buf = std::vector<std::byte>(static_cast<std::size_t>(len));
+
+        MPI_Recv(buf.data(), len, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+        const auto command = stat.MPI_TAG;
+        const auto payload = std::span<std::byte>(buf);
+
+        switch (command) {
+        case LINDA_RT_DB_SYNC_INSERT_TAG: {
+            const auto rx_inserted = deserialize(payload);
+            _store.out_nosignal(rx_inserted);
+            break;
         }
-        auto buf = std::make_unique<std::byte[]>(static_cast<std::size_t>(len));
-        MPI_Recv(buf.get(), len, MPI_CHAR, MPI_ANY_SOURCE, LINDA_RT_DB_SYNC_INSERT_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        auto rx_tuple = deserialize({buf.get(), static_cast<unsigned>(len)});
-        _store.out_nosignal(rx_tuple);
+
+        case LINDA_RT_DB_SYNC_DELETE_TAG: {
+            const auto rx_deleted = deserialize(payload);
+            // _store.remove_nosignal(rx_deleted); TODO
+            break;
+        }
+
+        case LINDA_RT_TERMINATE_TAG:
+            return;
+
+        default:
+            std::cerr << "ERROR: unknown command received ("
+                      << std::showbase << std::hex << command << std::dec << std::noshowbase
+                      << ")\n";
+        }
     }
 }
